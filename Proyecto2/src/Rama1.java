dcf4b5945942b82c055afc4c09209436e40b453c
entornosparaninfo <entornosparaninfo@gmail.com>
2021-12-06 15:53:58 +0100

    Segundo commit (este en rama1)

---
+++ Proyecto2/src/Rama1.java
@@ -0,0 +1,9 @@
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		System.out.println ("Primer mensaje de la rama1");
+		System.out.println ("Segungo mensaje de la rama1");
+	}
+
+}
